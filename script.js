fe6484f44ce58925c05b0a3f78535a06d06cfa23
Manoj_Dharmawardhana <manojanuradha00@gmail.com>
2023-12-20 08:51:28 +0530

    Started working on java script part

---
+++ script.js
@@ -0,0 +1,8 @@
+const ageCalculate = () => {
+
+const today = new Date();
+const inputDate = new Date (document.getElementById("date-input").value);
+
+}
+
+document.getElementById("calc-age-btn").addEventListener("click", ageCalculate);
